04b8f312fb67048c2e9d486e026b89381285a103
ZoeKisielewicz <49265159+ZoeKisielewicz@users.noreply.github.com>
2019-04-11 14:45:56 +1000

    Update main.c
    Added menu

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 
 int main() {
     char 
+    int i;
+    printf("Enter 1 to encript a rotation cypher\n");
+    printf("Enter 2 to decript a rotation cypher\n");
+    printf("Enter 3 to encript a substitution cypher\n");
+    printf("Enter 4 to decript a substitution cypher\n");
+    printf("Enter an integer: \n");
+    scanf("%d", &i);
 
 
  return;
